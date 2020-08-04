ca20c52093eb8193de6442e62544e30ceebb4df0
JPSS14 <jp_scarabelli@hotmail.com>
2020-08-03 23:28:59 -0300

    Your Database UPDATE 1: Added main header

---
+++ Your-database/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Your Database</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/Styles/boot.css">
+    <link rel="stylesheet" href="Themes/YourDatabaseTheme/style.css">
+</head>
+
+<body>
+    <header class="main_header">
+        <h1>Your Database</h1>
+        <nav class="main_header_nav">
+            <ul class="main_header_nav_ul">
+                <li class="main_header_nav_ul_li">DATABASE
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="create-database.php">CREATE</a></li>
+                        <li class="dropdown_item"><a href="update-database.php">UPDATE</a></li>
+                        <li class="dropdown_item"><a href="drop-database.php">DROP</a></li>
+                    </ul>
+                </li>
+                <li class="main_header_nav_ul_li">TABLE
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="create-table.php">CREATE</a></li>
+                        <li class="dropdown_item"><a href="update-table.php">UPDATE</a></li>
+                        <li class="dropdown_item"><a href="drop-table.php">DROP</a></li>
+                    </ul>
+                </li>
+                <li class="main_header_nav_ul_li">FIELD
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="insert-field.php">INSERT</a></li>
+                        <li class="dropdown_item"><a href="update-field.php">UPDATE</a></li>
+                        <li class="dropdown_item"><a href="delete-field.php">DELETE</a></li>
+                    </ul>
+                </li>
+                <li class="main_header_nav_ul_li">VIEW
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="create-view.php">CREATE</a></li>
+                        <li class="dropdown_item"><a href="update-view.php">UPDATE</a></li>
+                        <li class="dropdown_item"><a href="drop-view.php">DROP</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
